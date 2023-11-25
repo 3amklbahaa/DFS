9bdc2501d02d3bf45eaefea479e2d2552b23e45a
3amklbahaa <150846637+3amklbahaa@users.noreply.github.com>
2023-11-25 20:07:32 +0200

    Update dfs.c

--- dfs.c
+++ dfs.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-void fill_matrix(int **p,int N){
-    int i, j;
-    for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
-            printf("plz enter the T[%d][%d] elemet: ",i+1,j+1);
+void get_array(int **p, int N){
+    printf("plz enter the matrix: \n");
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
             scanf("%d",*(p+i)+j);
         }
     }
 }
-void print_matrix(int **p, int N){
-    int j,i;
-    for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
-            printf("%d",*(*(p+i)+j));
+void print_array(int **p,int N){
+    printf("the arrays is: \n");
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            printf("%d ",*(*(p+i)+j));
         }
         printf("\n");
     }
 }
-int dfs(int **p,int N,int start,int *visited){
-    printf("%d\t",start);
+int dfs(int **p, int N, int *visited, int start){
+    printf("%d ",start);
     visited[start]=1;
-    for(int j=0;j<N;j++){
-        if(*(*(p+start)+j)==1&&!visited[j]){
-            dfs(p,N,j,visited);
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(p[start][j]==1&&!visited[j]){
+                dfs(p,N,visited,j);
+            }
         }
     }
 }
 int main(){
-    int **p,N,i,start,*visited;
-    printf("plz enter N: ");
+    int **p, N, *visited, start;
+    printf("plz enter N(number of vertices): ");
     scanf("%d",&N);
-    printf("plz enter where u start: ");
-    scanf("%d",&start);
     p=(int **)malloc(N*sizeof(int *));
-    for(i=0;i<N;i++){
+    for(int i=0;i<N;i++){
         *(p+i)=(int *)malloc(N*sizeof(int));
     }
-    visited=(int*)malloc(N*sizeof(int));
-    fill_matrix(p,N);
-    print_matrix(p,N);
-    dfs(p,N,start,visited);
+    visited=(int *)malloc(N*sizeof(int));
+    printf("plz enter start: ");
+    scanf("%d",&start);
+    for (int i = 0; i < N; i++) {
+        visited[i] = 0;
+    }
+    get_array(p,N);
+    print_array(p,N);
+    printf("result: ");
+    dfs(p,N,visited,start);
+    return 0;
 }
+//try 1 1 0, 0 1 1, 1 0 0
